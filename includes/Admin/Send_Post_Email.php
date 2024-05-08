41a06341ce76b165fc945f2d9884d50c8ebce9a5
ashique <ashique12009@gmail.com>
2024-05-08 09:05:58 +0300

    email type as html

--- includes/Admin/Send_Post_Email.php
+++ includes/Admin/Send_Post_Email.php
@@ -5,20 +5,35 @@ class Send_Post_Email
 {
     public function __construct()
     {
-        add_action('save_post_post', [$this, 'wpdocs_notify_subscribers'], 10, 3);
+        add_action('save_post_post', [$this, 'send_notification_to_subscribers'], 10, 3);
+        add_filter('wp_mail_content_type', [$this, 'pnl_wp_mail_content_type']);
     }
 
-    public function wpdocs_notify_subscribers($post_id, $post, $update) {
+    public function send_notification_to_subscribers($post_id, $post, $update) 
+    {
         // If an old post is being updated, exit
         if ($update) {
             return;
         }
 
-        $subscribers = ['fin.ashique@gmail.com', 'ashique12009@gmail.com']; // list of your subscribers
+        // Get the list of subscribers
+        global $wpdb;
+        $table_prefix = $wpdb->prefix;
+        $sql          = "SELECT email_address FROM {$table_prefix}postnewsletter_emails";
+        $subscribers  = $wpdb->get_col($sql);
+
+        if (empty($subscribers)) {
+            return;
+        }
+
         $subject     = 'A new post has beed added!';
         $message     = sprintf('We\'ve added a new post. Click <a href="%s">here</a> to see the post', get_permalink($post));
 
         wp_mail($subscribers, $subject, $message); 
     }
 
+    public function pnl_wp_mail_content_type() 
+    {
+        return 'text/html';
+    }
 }
